a4f3ed61702c2a7c01c8183dfe43fe85fbf1d187
Rennan Silva <jrennan99@gmail.com>
2024-10-04 16:33:36 -0300

    feat: add data

---
+++ src/data/projects.ts
@@ -0,0 +1,88 @@
+export const projects = [
+  {
+    src: "https://i.imgur.com/CtZxGFx.png",
+    alt: "Vista-geek",
+    name: "E-commerce",
+    description: "Template de um e-commerce de roupas",
+    link: "https://vista-geek.vercel.app/",
+    type: 1,
+  },
+  {
+    src: "https://i.imgur.com/KXZAel9.png",
+    alt: "Recipes-site",
+    name: "Site de Receitas",
+    description: "Template de um site de receitas",
+    link: "https://recipes-site-ecru.vercel.app/",
+    type: 2,
+  },
+  {
+    src: "https://i.imgur.com/hubBoDI.png",
+    alt: "Landing-page",
+    name: "Template Portfólio",
+    description: "Portfólio de programação customizavél",
+    link: "https://next-portfolio-template-rho.vercel.app/",
+    type: 3,
+  },
+  {
+    src: "https://i.imgur.com/0kbw5Ep.png",
+    alt: "Portfolio",
+    name: "Portfólio",
+    description:
+      "Meu primeiro portfólio, lá estão vários projetos mais básicos e é um bom comparativo da minha evolução!",
+    link: "https://rennan20.github.io/meu-portfolio/",
+    type: 4,
+  },
+  {
+    src: "https://i.imgur.com/rz9fUdf.png",
+    alt: "Calculadora IMC",
+    name: "Calculadora IMC",
+    description: "Calculadora de IMC simples feita em React com Vite.",
+    link: "https://react-calc-imc-snowy.vercel.app/",
+    type: 5,
+  },
+  {
+    src: "https://i.imgur.com/CJci8qK.png",
+    alt: "RPG",
+    name: "Mini RPG",
+    description:
+      "Interação de diversos elementos no estilo RPG, feito em React",
+    link: "https://rpg-tag.vercel.app/",
+    type: 6,
+  },
+  {
+    src: "https://i.imgur.com/6Pi9w4k.png",
+    alt: "NFT Droper",
+    name: "NFT Droper",
+    description:
+      "Aplicativo para gerar NFT, o usuário pode logar com MetaMask e gerar uma NFT da coleção salva no Sanity e ThirdWeb",
+    link: "https://nft-drop-mu-lilac.vercel.app/",
+    type: 7,
+  },
+  {
+    src: "https://i.imgur.com/EQH5IAg.png",
+    alt: "ChatGPT",
+    name: "Clone ChatGPT",
+    description:
+      "Clone do chatGPT usando a API da OpenAI, o usuário pode fazer login com Google, os dados são armazenados com Firebase",
+    link: "https://chatgpt-clone-hazel.vercel.app/",
+    type: 8,
+  },
+  {
+    src: "https://i.imgur.com/9yKXCOX.png",
+    alt: "Clone Netflix",
+    name: "Clone Netflix",
+    description:
+      "Clone da Netflix, onde o usuário pode se cadastrar e acessar a tela de filmes, utiliza Firebase para manter os dados de login e carrega os filmes através de uma API externa",
+    link: "https://netflix-clone-51cb3.web.app/",
+    type: 9,
+  },
+  {
+    src: "https://i.imgur.com/qUco6UV.png",
+    alt: "Gerenciador Financeiro",
+    name: "Manager",
+    description:
+      "Aplicativo onde o usuário pode fazer o gerenciamento dos seus gastos e calcular o total no mês, feito com React",
+    link: "https://expense-manager-beryl.vercel.app/",
+    type: 10,
+  },
+];
