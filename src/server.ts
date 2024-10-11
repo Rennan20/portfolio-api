9882cd00a8712c85234a4515de4e471c517427ba
Rennan Silva <jrennan99@gmail.com>
2024-10-11 15:41:46 -0300

    feat: add dotenv

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import projectRoutes from "./routes/projectRoutes";
 import templateRoutes from "./routes/templateRoutes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
